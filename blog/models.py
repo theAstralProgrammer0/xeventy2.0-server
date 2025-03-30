93a21d7e085eeb0b08580164b492edd76457ef53
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-03-30 11:08:35 +0100

    Added new Django app (blog/)

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+"""
+This model stores the title, description, image_src, blog_url, and 
+created_at field for ordering articles
+"""
+
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image_src = models.URLField(max_length=500)
+    blog_url = models.URLField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
