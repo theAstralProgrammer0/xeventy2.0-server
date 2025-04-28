42b5e4fd712ad580125ae4c1b516eaed71e02a59
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-04-28 05:57:57 +0100

    UPDATED: requirements.txt

--- training/urls.py
+++ training/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import VideoListCreateView, VideoRetrieveView, zoom_join
 
 urlpatterns = [
-    path('videos/', VideoListCreateView.as_view(), name='video-list');
+    path('videos/', VideoListCreateView.as_view(), name='video-list'),
     path('videos/<int:pk>/', VideoRetrieveView.as_view(), name='video-detail'),
     path('videos/<int:pk>/zoom/', zoom_join, name='video-zoom-join'),
 ]
